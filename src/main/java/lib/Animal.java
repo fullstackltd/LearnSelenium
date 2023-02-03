158dbcc5cde672b3db4ba3504a4807e1d5bac104
fullstackltd <fullstackltd@gmail.com>
2023-02-03 20:11:54 +0600

    lib

---
+++ src/main/java/lib/Animal.java
@@ -0,0 +1,5 @@
+package lib;
+
+public abstract class Animal {
+    public abstract void move();
+}
---
+++ src/main/java/lib/Cat.java
@@ -0,0 +1,8 @@
+package lib;
+
+public class Cat extends Animal{
+    @Override
+    public void move() {
+        System.out.println("Cat is moving...");
+    }
+}
